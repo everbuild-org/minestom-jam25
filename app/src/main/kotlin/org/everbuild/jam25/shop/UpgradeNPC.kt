5b9987151623a8890858aa03d7efa0964c222fdb
Christian Bergschneider <christian.bergschneider@gmx.de>
2025-07-18 10:17:17 +0200

    feat: update `UpgradeNPC` shop with metal scraps and crafting item stack
    - Add `MetalScrapsItem` with new stack creation in `UpgradeNPC` shop inventory.
    - Integrate `spyglass` as a placeholder crafting item within the shop menu.

--- app/src/main/kotlin/org/everbuild/jam25/shop/UpgradeNPC.kt
+++ app/src/main/kotlin/org/everbuild/jam25/shop/UpgradeNPC.kt
@@ -4,6 +4,12 @@ import java.util.concurrent.CompletableFuture
 import net.minestom.server.coordinate.Pos
 import net.minestom.server.entity.Player
 import net.minestom.server.instance.Instance
+import net.minestom.server.item.ItemStack
+import net.minestom.server.item.Material
+import org.everbuild.jam25.item.impl.DigitalComponentItem
+import org.everbuild.jam25.item.impl.DigitalCrafterBlockItem
+import org.everbuild.jam25.item.impl.MetalScrapsItem
+import org.everbuild.jam25.item.impl.SiliconItem
 
 class UpgradeNPC(val pos: Pos) : ShopNPC("Upgrades", SKIN_ENGINEER) {
     override fun setInstance(instance: Instance): CompletableFuture<Void?>? {
@@ -12,7 +18,7 @@ class UpgradeNPC(val pos: Pos) : ShopNPC("Upgrades", SKIN_ENGINEER) {
 
     override fun openMenu(player: Player) {
         player.openInventory(ShopGUI("Upgrades", listOf(
-
+            ShopGUI.ShopEntry.Item(MetalScrapsItem.createNewStack(2),null, ItemStack.of(Material.SPYGLASS)),
         )))
     }
 }
